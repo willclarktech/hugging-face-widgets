e1d54877c92e6a4e8f2b3b08f484263158ca2915
willclarktech <willclarktech@users.noreply.github.com>
2020-07-17 14:57:13 +0200

    :seedling: Connect GenderBiasWidget to App/index

---
+++ src/containers/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import GenderBiasWidgetContainer from "./GenderBiasWidgetContainer";
+
+class App extends Component {
+	render(): JSX.Element {
+		return <GenderBiasWidgetContainer />;
+	}
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 
-import App from "./components/App";
+import App from "./containers/App";
 
 render(<App />, document.getElementById("app"));
