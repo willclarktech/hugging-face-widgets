daceeeeadb666c42ba89e8d07704ec905e2e0fc5
willclarktech <willclarktech@users.noreply.github.com>
2020-07-17 12:29:27 +0200

    :seedling: Add utils

---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+export const post = (url: string, params: RequestInit = {}): Promise<unknown> =>
+	fetch(url, { ...params, method: "POST" }).then((response) => {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		return response.json();
+	});
+
+export function assert(condition: boolean, msg?: string): asserts condition {
+	if (!condition) {
+		throw new Error(msg || "condition is not truthy");
+	}
+}
+
+const femaleWords = ["she", "her"];
+const maleWords = ["he", "him", "his"];
+const genderedWords = [...femaleWords, ...maleWords];
+
+const startRegExp = "(^|\\s)";
+const endRegExp = "($|\\s)";
+export const femaleTokenRegExp = new RegExp(
+	femaleWords.map((word) => `^${word}$`).join("|"),
+	"i",
+);
+export const maleTokenRegExp = new RegExp(
+	maleWords.map((word) => `^${word}$`).join("|"),
+	"i",
+);
+export const genderedElementRegExp = new RegExp(
+	genderedWords.map((word) => `${startRegExp}${word}${endRegExp}`).join("|"),
+	"gi",
+);
+
+export const mask = (text: string): string =>
+	text.replace(genderedElementRegExp, " <mask> ");
